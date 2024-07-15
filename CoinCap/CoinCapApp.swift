5156b77138366734b5f2c56104270f8c3392b170
Dmitriy Yarmolchuk <dmitriy.yarmolchuk@fintech-farm.com>
2024-07-15 17:30:07 +0300

    Initial Commit

---
+++ CoinCap/CoinCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCapApp.swift
+//  CoinCap
+//
+//  Created by Dmytro Yarmolchuk on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
